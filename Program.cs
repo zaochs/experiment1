884b7f5d0c9fe9b7734b40d7934c82909f88ac02
zaop <11255568+enteryournamehere@users.noreply.github.com>
2023-11-13 00:17:29 +0100

    read all files in folder

--- Program.cs
+++ Program.cs
@@ -1,53 +1,99 @@
 using System.Xml.Serialization;
 
-var path = "path/to/skane/line_12_802_9011012080200000.xml";
-
-Console.WriteLine($"Loading file {path}");
-
-var mySerializer = new XmlSerializer(typeof(Netex.PublicationDeliveryStructure));
-
-using var myFileStream = new FileStream(path, FileMode.Open);
-
-var myObject = (Netex.PublicationDeliveryStructure)mySerializer.Deserialize(myFileStream)!;
-foreach (var compositeFrame in myObject.DataObjects.CompositeFrame)
+namespace NetexTest
 {
-    Console.WriteLine("CompositeFrame");
-    foreach (var serviceFrame in compositeFrame.Frames.ServiceFrame)
+    class Program
     {
-        Console.WriteLine("- ServiceFrame");
-        foreach (var line in serviceFrame.Lines.Line)
+        static void Main(string[] args)
         {
-            var name = line.Name.Value;
-            Console.WriteLine($"  - Line {name} (ID {line.Id})");
-        }
+            Console.WriteLine($"Current working directory: {Environment.CurrentDirectory}");
 
-        foreach (var route in serviceFrame.Routes.Route)
-        {
-            var points = route.PointsInSequence.PointOnRoute;
-            var name = route.Name.Value;
-            var directionType = route.DirectionType.ToString();
-        
-            Console.WriteLine($"  - Route {name} ({directionType}), {points.Count} stops (part of line {route.LineRef.Ref})");
-        }
-
-        foreach (var journeyPattern in serviceFrame.JourneyPatterns.JourneyPattern)
-        {
-            var stops = journeyPattern.PointsInSequence.StopPointInJourneyPattern;
-            Console.WriteLine($"  - JourneyPattern: {stops.Count} stop points (ID: {journeyPattern.Id})");
+            // read the netex directory from netex_dir.txt
+            String netexDir;
+            try
+            {
+                netexDir = File.ReadAllText("netex_dir.txt").Trim();
+            }
+            catch (Exception e)
+            {
+                Console.Write("Could not read netex_dir.txt: ");
+                Console.WriteLine(e.Message);
+                return;
+            }
+            
+            // find all files in there
+            var files = Directory.GetFiles(netexDir, "*.xml");
+            Console.WriteLine($"Number of files: {files.Length}");
+            
+            var parser = new Parser();
+            
+            // loop through
+            foreach (var file in files)
+            {
+                parser.ParseFile(file);
+            }
         }
     }
 
-    foreach (var timetableFrame in compositeFrame.Frames.TimetableFrame)
+    class Parser
     {
-        Console.WriteLine("- TimetableFrame");
+        readonly XmlSerializer _mySerializer = new XmlSerializer(typeof(Netex.PublicationDeliveryStructure));
 
-        var serviceJourneys = timetableFrame.VehicleJourneys.ServiceJourney;
-        Console.WriteLine($"  - Number of ServiceJourneys: {serviceJourneys.Count}");
-        foreach (var serviceJourney in serviceJourneys)
+        public void ParseFile(String path)
         {
-            // Console.WriteLine("  - ServiceJourney");
+            Console.WriteLine($"Loading file {path}");
+
+            using var myFileStream = new FileStream(path, FileMode.Open);
+
+            var myObject = (Netex.PublicationDeliveryStructure)_mySerializer.Deserialize(myFileStream)!;
+            foreach (var compositeFrame in myObject.DataObjects.CompositeFrame)
+            {
+                Console.WriteLine("CompositeFrame");
+                foreach (var serviceFrame in compositeFrame.Frames.ServiceFrame)
+                {
+                    // for shared_data.xml,
+                    // Lines, Routes and JourneyPatterns do not exist
+                    
+                    Console.WriteLine("- ServiceFrame");
+                    if (serviceFrame.Lines != null)
+                        foreach (var line in serviceFrame.Lines.Line)
+                        {
+                            var name = line.Name.Value;
+                            Console.WriteLine($"  - Line {name} (ID {line.Id})");
+                        }
+                    
+                    
+                    if (serviceFrame.Routes != null)
+                        foreach (var route in serviceFrame.Routes.Route)
+                        {
+                            var points = route.PointsInSequence.PointOnRoute;
+                            var name = route.Name.Value;
+                            var directionType = route.DirectionType.ToString();
+
+                            Console.WriteLine(
+                                $"  - Route {name} ({directionType}), {points.Count} stops (part of line {route.LineRef.Ref})");
+                        }
+
+                    if (serviceFrame.JourneyPatterns != null)
+                        foreach (var journeyPattern in serviceFrame.JourneyPatterns.JourneyPattern)
+                        {
+                            var stops = journeyPattern.PointsInSequence.StopPointInJourneyPattern;
+                            Console.WriteLine($"  - JourneyPattern: {stops.Count} stop points (ID: {journeyPattern.Id})");
+                        }
+                }
+
+                foreach (var timetableFrame in compositeFrame.Frames.TimetableFrame)
+                {
+                    Console.WriteLine("- TimetableFrame");
+
+                    var serviceJourneys = timetableFrame.VehicleJourneys.ServiceJourney;
+                    Console.WriteLine($"  - Number of ServiceJourneys: {serviceJourneys.Count}");
+                    foreach (var serviceJourney in serviceJourneys)
+                    {
+                        // Console.WriteLine("  - ServiceJourney");
+                    }
+                }
+            }
         }
     }
 }
-
-
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # usage
 1. get some data like the files from Skåne
-2. edit the path in Program.cs
-3. `dotnet run`
+2. in whichever working directory you will run this, create netex_dir.txt
+3. in there put the path to the folder with netex files
+4. `dotnet run`
 
 # info
 Gml.cs and Netex.cs generated using https://github.com/mganss/XmlSchemaClassGenerator + https://github.com/NeTEx-CEN/NeTEx-light
